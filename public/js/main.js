6f5ab17483166547f0f34fb65f7f889ab7af8018
sammysam1210 <smliu210@yahoo.com>
2017-11-21 08:17:17 -0800

    fixed background issue

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,11 @@
 var picCount = 0;
 var picArray = ["images/background.jpg","images/background2.jpg"];
 var x = document.getElementsByTagName("html");
-x[0].style.backgroundImage=localStorage.bgImg;
+if (localStorage.bgImg != "")
+  x[0].style.backgroundImage=localStorage.bgImg;
+else {
+  x[0].style.backgroundImage='url("images/background.jpg")';
+}
 function changeBack()
 {
     if (x[0].style.backgroundImage == 'url("images/background2.jpg")'){
