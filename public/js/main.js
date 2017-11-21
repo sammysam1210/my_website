b19e7d2baa38fe32c079253ca4fc0abce59293a0
sammysam1210 <smliu210@yahoo.com>
2017-11-20 22:20:03 -0800

    background change added, js added

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+var picCount = 0;
+var picArray = ["images/background.jpg","images/background2.jpg"];
+var x = document.getElementsByTagName("html");
+x[0].style.backgroundImage=localStorage.bgImg;
+function changeBack()
+{
+    if (x[0].style.backgroundImage == 'url("images/background2.jpg")'){
+      localStorage.bgImg = 'url("images/background.jpg")';
+      x[0].style.backgroundImage="url(images/background.jpg)";
+    }
+    else if (x[0].style.backgroundImage == 'url("images/background.jpg")' || x[0].style.backgroundImage ==""){
+      localStorage.bgImg = 'url("images/background2.jpg")';
+      x[0].style.backgroundImage="url(images/background2.jpg)";
+    }
+}
